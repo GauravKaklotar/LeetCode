54eeaebdf0a604a334c8a34add1867443316234b
Gaurav Kaklotar <gaurav.kaklotar03@gmail.com>
2023-11-23 17:34:47 +0530

    Time: 168 ms (50.56%), Space: 16.7 MB (6.01%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1630-arithmetic-subarrays/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1630-arithmetic-subarrays.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        ans = []
+        n, m = len(nums), len(l)
+        for i in range(m):
+            temp = nums[l[i]:r[i]+1]
+            length = r[i] - l[i] + 1
+            if length >= 2:
+                temp.sort()
+                diff = temp[1]-temp[0]
+                flag = True
+                for i in range(1, len(temp)):
+                    if temp[i] - temp[i-1] != diff:
+                        flag = False
+                        ans.append(False)
+                        break
+                if flag == True: ans.append(True)
+            else: ans.append(True)
+        return ans            
